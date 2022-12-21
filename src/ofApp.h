f790c3b02e85ebb92943def06e5ef59e35322436
Kacy James <inquen@gmail.com>
2022-12-21 00:59:14 -0500

    Added some test code

--- src/ofApp.h
+++ src/ofApp.h
@@ -10,15 +10,11 @@ public:
   void draw();
   void exit();
 
-//  void touchdown(oftoucheventargs & touch);
-//  void touchmoved(oftoucheventargs & touch);
-//  void touchup(oftoucheventargs & touch);
-//  void touchdoubletap(oftoucheventargs & touch);
-//  void touchcancelled(oftoucheventargs & touch);
-//
   void lostfocus();
   void gotfocus();
   void gotmemorywarning();
   
+  ofEasyCam cam;
+  ofMesh mesh;
 };
 
